c4f92a257e1da847bf108e9866a7a3ebfcd0414e
Faridpro1 <94738065+Faridpro1@users.noreply.github.com>
2022-05-14 22:04:26 +0400

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useState } from 'react'
+import Button from "./componnet"
+
+const App = () => {
+  return (
+    <button bg="yellow">Login</button>
+  )
+}
+
+export default App
+
---
+++ button.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const button = ({children,bg,color}) => {
+    style:{{backgroundcolor = bg, color =  color}}
+    {children}
+}
+
+export default button
